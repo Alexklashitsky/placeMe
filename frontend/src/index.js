7b08b5fd9f2a98d276bfe8ad7b6ca71fd8ffa117
LoubtaonAv <aegtar88@gmail.com>
2022-01-19 09:50:39 +0200

    iajsd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/scss/main.scss';
-import { RootCmp } from './assets/CmpRoot';
+import { RootCmp } from './assets/RootCmp';
 import { Provider } from 'react-redux';
 
-import { store } from './js/store/store'
+import { store } from './js/store/store';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,4 +13,4 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,28 +1,22 @@
+// import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
+// import thunk from 'redux-thunk'
 
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
-import thunk from 'redux-thunk'
+// // const { createStore, combineReducers } = Redux
+// // const { applyMiddleware } = Redux
+// // const thunk = ReduxThunk.default
 
+// import { toysReducer } from "./toy.reducer.js";
+// import { userReducer } from "./user.reducer.js";
 
-// const { createStore, combineReducers } = Redux
-// const { applyMiddleware } = Redux
-// const thunk = ReduxThunk.default
+// const rootReducer = combineReducers({
+//     toysModule: toysReducer,
+//     userModule: userReducer
+// })
 
-import { toysReducer } from "./toy.reducer.js";
-import { userReducer } from "./user.reducer.js";
+// export default createStore(
+//     rootReducer,
+//     applyMiddleware(thunk)
+// )
 
-const rootReducer = combineReducers({
-    toysModule: toysReducer,
-    userModule: userReducer
-})
-
-
-
-export default createStore(
-    rootReducer,
-    applyMiddleware(thunk)
-)
-
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
-
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
