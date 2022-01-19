9c29ebc6a883202cc9f8ce0aed15c9a4a96cfc50
Your Name <alexander.klashitsky@gmail.com>
2022-01-19 22:00:58 +0200

    22:10

--- frontend/src/cmps/StayFilterContainer.jsx
+++ frontend/src/cmps/StayFilterContainer.jsx
@@ -1,16 +1,27 @@
 import { BentoTwoTone } from '@mui/icons-material'
 import { Component } from 'react'
 import { connect } from 'react-redux'
+import { Button } from "@material-ui/core";
 
 
 export class StayFilter extends Component {
-    state = {}
 
+    state = {
+        currModalShown: ''
+    }
+
+    onOpenModal(value) {
+        this.setState({ currModalShown: value }, () => console.log('this.state:', this.state))
+
+
+
+
+    }
 
     render() {
         return (
             <section className='filter-header flex' >
-                <ul className='special-stay flex '>
+                <ul className='special-stay flex clean-list'>
                     <li>Houseboats</li>
                     <li>Beachfront</li>
                     <li>Cabins</li>
@@ -21,11 +32,26 @@ export class StayFilter extends Component {
 
                 </ul>
 
-                <div className='round-button'></div>
-                <button>Anytime </button>
-                <button>Guests</button>
-                <button>Filters</button>
-            </section>
+                <div className='round-button'>
+                    {/* <Button variant="outlined">Primary</Button> */}
+                    <button onClick={() => this.onOpenModal('cal')} >Anytime </button>
+                    <button onClick={() => this.onOpenModal('guests')}  >Guests</button>
+                    <button onClick={() => this.onOpenModal('filters')} >Filters</button>
+                </div>
+                <div className={"cal-modal" + " " + `${this.state.currModalShown === 'cal' ? " " : "shown"}`}>
+
+                </div>
+                <div className={"guests-modal" + " " + `${this.state.currModalShown === 'guests' ? " " : "shown"}`}>
+
+                </div>
+                <div className={"filters-modal" + " " + `${this.state.currModalShown === 'filters' ? " " : "shown"}`}>
+
+                </div>
+
+
+
+
+            </section >
         )
 
     }
